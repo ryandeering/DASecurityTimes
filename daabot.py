832ca6bb1df75ca695a8f2105c1048dce6874bae
Ryan Deering <ryandeering1@gmail.com>
2024-01-12 14:36:54 +0000

    Initial version (not linited or PEP8 compliant)

--- daabot.py
+++ daabot.py
@@ -1,75 +1,109 @@
 import calendar
 import re
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 from io import BytesIO
-from time import sleep, strftime
 
 import requests
 import tweepy
 from bs4 import BeautifulSoup
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point, WriteOptions
+from influxdb_client.client.write_api import SYNCHRONOUS
 from PIL import Image
 
-from credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+from credentials import (
+    CONSUMER_KEY,
+    CONSUMER_SECRET,
+    ACCESS_TOKEN,
+    ACCESS_TOKEN_SECRET,
+    INFLUXDB_URL,
+    INFLUXDB_BUCKET,
+    INFLUXDB_ORG,
+    INFLUXDB_TOKEN,
+)
 from daaplot import show_plot
 
-
-def uploadimage():
-    sleep(5)  # let i/o do it's thing
-    img = Image.open("chart.png")
-    b = BytesIO()
-    img.save(b, "PNG")
-    b.seek(0)
-    ret = api.media_upload(filename="foo", file=b)  # filename doesn't matter
-    return ret
+def init_twitter_api():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    return tweepy.API(auth)
 
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+def init_influxdb_client():
+    return InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 
-today = datetime.now()
-date_time = today.strftime("%d/%m/%Y %H:%M")
 
-influx_client = InfluxDBClient(host="localhost", port=8086)
-PLOT_SHOWN = None
+def get_airport_times():
+    URL = "https://www.dublinairport.com/flight-information/live-departures"
+    html_doc = requests.get(URL).content
+    soup = BeautifulSoup(html_doc, "html.parser")
+    times = soup.find("div", class_="sec-times").findAll("strong")
+    t1 = re.findall(r"\d+", times[0].getText())[0]
+    t2 = re.findall(r"\d+", times[1].getText())[0]
+    return t1, t2
 
-URL = "https://www.dublinairport.com/flight-information/live-departures"
-html_doc = requests.get(URL).content
-soup = BeautifulSoup(html_doc, "html.parser")
-times = soup.find("div", class_="sec-times").findAll("strong")
-t1 = re.findall(r"\d+", times[0].getText())
-t2 = re.findall(r"\d+", times[1].getText())
 
-tweet = "Current times - " + calendar.day_name[today.weekday()] + ", " + date_time + " " + "\nTerminal 1: " + str(
-    t1[0]) + " minutes" + "\nTerminal 2: " + str(t2[0]) + " minutes"
+def write_to_influxdb(influx_client, t1, t2):
+    point = Point("terminals").field("T1", int(t1)).field("T2", int(t2))
+    write_api = influx_client.write_api(write_options=SYNCHRONOUS)
+    write_api.write(bucket=INFLUXDB_BUCKET, record=point)
 
-if t1[0] == "60" or t2[0] == "60":
-    tweet += "\n\nWARNING: Allow yourself extra time to get through security. A terminal is peaking. "
 
-if t1[0] or t2[0]:
-    influx_client.write(
-        ["terminals T1=" + t1[0] + ",T2=" + t2[0]], {"db": "DAA"}, 204, "line"
-    )
+def generate_tweet(today, t1, t2):
+    date_time = today.strftime("%d/%m/%Y %H:%M")
+    tweet = f"Current times - {calendar.day_name[today.weekday()]}, {date_time} \nTerminal 1: {t1} minutes\nTerminal 2: {t2} minutes"
 
-if strftime("%H:%M") == "12:00":
+    if t1 == "60" or t2 == "60":
+        tweet += "\n\nWARNING: Allow yourself extra time to get through security. A terminal is peaking."
+
+    return tweet
+
+
+def plot_and_upload():
+    current_time = datetime.now().strftime("%H:%M")
     date = datetime.today() - timedelta(days=1)
-    show_plot(
-        date.strftime("%d/%m/%Y")
-        + " - "
-        + datetime.today().strftime("%d/%m/%Y")
-        + " Security Times"
-    )
-    PLOT_SHOWN = uploadimage()
-    tweet += "\n\n #DAA #DublinAirport"
+    plot_title = None
+    plot_media = None
 
-if strftime("%H:%M") == "00:00":
-    date = datetime.today() - timedelta(days=1)
-    show_plot(date.strftime("%d/%m/%Y") + " Security Times")
-    PLOT_SHOWN = uploadimage()
-    tweet += "\n\n #DAA #DublinAirport"
+    if current_time == "12:00":
+        plot_title = (
+            date.strftime("%d/%m/%Y")
+            + " - "
+            + datetime.today().strftime("%d/%m/%Y")
+            + " Security Times"
+        )
+        plot_media = show_plot(plot_title)
+    elif current_time == "00:00":
+        plot_title = date.strftime("%d/%m/%Y") + " Security Times"
+        plot_media = show_plot(plot_title)
 
-if PLOT_SHOWN is None:
-    api.update_status(tweet)
-else:
-    api.update_status(media_ids=[PLOT_SHOWN.media_id_string], status=tweet)
+    return plot_media
+
+def upload_tweet(api, text, image_buffer=None):
+    try:
+        if image_buffer:
+            image_buffer.seek(0)
+            uploaded_media = api.media_upload(filename='plot.png', file=image_buffer)
+            api.update_status(status=text, media_ids=[uploaded_media.media_id_string])
+        else:
+            api.update_status(status=text)
+    except Exception as e:
+        print("An error occurred while posting the tweet:", e)
+
+def main():
+    api = init_twitter_api()
+    influx_client = init_influxdb_client()
+    t1, t2 = get_airport_times()
+
+    today = datetime.now()
+    tweet_text = generate_tweet(today, t1, t2)
+
+    if t1 is not None and t2 is not None:
+        write_to_influxdb(influx_client, t1, t2)
+
+    plot_media = plot_and_upload()
+
+    upload_tweet(api, tweet_text, plot_media)
+
+
+if __name__ == "__main__":
+    main()
--- daaplot.py
+++ daaplot.py
@@ -4,54 +4,86 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from dateutil import parser
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient
+from io import BytesIO
+from credentials import INFLUXDB_URL, INFLUXDB_BUCKET, INFLUXDB_ORG, INFLUXDB_TOKEN
 
+def init_influxdb_client():
+    return InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 
-def influx_client(database=None):
-    return InfluxDBClient(host="localhost", port="8086", database=database)
+def fetch_data(client):
+    query_api = client.query_api()
+    query = f'from(bucket: "{INFLUXDB_BUCKET}") |> range(start: -24h) |> filter(fn: (r) => r._measurement == "terminals")'
+    return query_api.query(org=INFLUXDB_ORG, query=query)
 
+def prepare_dataframe(result):
+    records = []
 
-def show_plot(titlename):
+    for table in result:
+        for record in table.records:
+            record_data = {
+                "_time": record.get_time(),
+                "_value": record.get_value(),
+                "_field": record.get_field(),
+            }
+            records.append(record_data)
 
+    df = pd.DataFrame(records)
+
+    if not df.empty:
+        df["time"] = pd.to_datetime(df["_time"])
+        df.rename(
+            columns={"_field": "terminal", "_value": "time_duration"}, inplace=True
+        )
+
+    return df
+
+def plot_data(df, titlename):
     matplotlib.rcParams["timezone"] = "Europe/Dublin"
-
-    result = influx_client(database="DAA").query(
-        "SELECT * FROM terminals WHERE time > now()-24h"
-    )
-
-    df = pd.DataFrame(columns=["measurement", "time", "T1", "T2"])
-
-    for k, v in result.items():
-        data = {"measurement": k[0]}
-        for p in v:
-            data.update({"time": parser.parse(p["time"]), "T1": p["T1"], "T2": p["T2"]})
-            df = df.append(data, ignore_index=True)  # convert to Pandas dataframe
-
-    fig = plt.figure(figsize=(12, 8))
-
     sns.set(style="darkgrid", context="talk")
     plt.style.use("dark_background")
+    fig, ax = plt.subplots(figsize=(12, 8))
 
-    plt.plot(df.time, df.T1, label="Terminal 1", linewidth=3, color="magenta")
+    terminal_colors = {"T1": "magenta", "T2": "cyan"}
 
-    plt.plot(df.time, df.T2, label="Terminal 2", linewidth=3, color="cyan")
+    for terminal, color in terminal_colors.items():
+        terminal_data = df[df["terminal"] == terminal]
+        if not terminal_data.empty:
+            ax.plot(
+                terminal_data["time"],
+                terminal_data["time_duration"],
+                label=f"{terminal}",
+                linewidth=3,
+                color=color,
+            )
 
-    plt.gca().spines["top"].set_visible(False)
-    plt.gca().spines["right"].set_visible(False)
-
-    y_ticks = np.arange(5, 65, 5)  # 5-60 range, ticks in 5
-    plt.yticks(y_ticks)
-
-    hours = mdates.HourLocator(interval=2)
-    my_fmt = mdates.DateFormatter("%H:%M")
-    plt.gca().xaxis.set_major_locator(hours)
-    plt.gca().xaxis.set_major_formatter(my_fmt)
-
-    plt.title(titlename)
-
-    plt.legend()
-
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.set_yticks(np.arange(5, 65, 5))
+    ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    ax.set_title(titlename)
+    ax.legend()
     plt.tight_layout()
 
-    plt.savefig("chart.png")
+    return fig
+
+def save_plot_to_buffer(fig):
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    buf.seek(0)
+    plt.close(fig)
+    return buf
+
+def show_plot(titlename):
+    client = init_influxdb_client()
+    try:
+        result = fetch_data(client)
+        df = prepare_dataframe(result)
+        if df.empty:
+            return None
+        fig = plot_data(df, titlename)
+        plot_buffer = save_plot_to_buffer(fig)
+        return plot_buffer
+    finally:
+        client.close()
